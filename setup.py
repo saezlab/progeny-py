9e0e6b5cf8c20c57a06e6d2be0ba02db129f7f93
Pau Badia i Mompel <paubadiamompel@gmail.com>
2021-05-10 09:50:57 +0200

    Added to pip

--- setup.py
+++ setup.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python
-import setuptools
+
+from distutils.core import setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-    
-setuptools.setup(
-    name="progeny-py",
-    version="1.0.1",
-    author="Pau Badia i Mompel",
+
+setup(
+    name='progeny-py',
+    version='1.0.2',
+    author='Pau Badia i Mompel',
     author_email="pau.badia@uni-heidelberg.de",
-    description="progeny-py is a python package to compute pathway activity \
-    from RNA-seq data using PROGENy",
+    description='progeny-py is a python package to compute pathway activity \
+    from RNA-seq data using PROGENy',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/saezlab/progeny-py",
+    url='https://github.com/saezlab/progeny-py',
     project_urls={
         "Bug Tracker": "https://github.com/saezlab/progeny-py/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "progeny"},
-    packages=setuptools.find_packages(where="progeny"),
-    python_requires=">=3.6",
+    packages=['progeny'], 
+    license='LICENSE.txt',
     package_data={
        'progeny': ['data/model_human_full.pkl', 
                    'data/model_mouse_full.pkl']
